2a71744221f4fcf0a6ac8fe6a9020839d61d9666
ianthomas0 <ianthomas0@live.com>
2023-06-29 14:16:08 -0400

    Fix source property that is getting set to null on insert (#8)

--- lib/scrapers/ligonier-scraper.ts
+++ lib/scrapers/ligonier-scraper.ts
@@ -1,12 +1,11 @@
-import { SermonScraper } from "./sermon-scraper";
+import { SermonScraper } from './sermon-scraper';
 import * as request from 'request-promise-native';
-import { Sermon } from "../models";
-import { Context } from "@azure/functions";
+import { Sermon } from '../models';
+import { Context } from '@azure/functions';
 
 const cheerio = require('cheerio');
 
 export class LigonierScraper implements SermonScraper {
-
     public source: string = 'Ligonier Ministries';
 
     public async scrape(context: Context): Promise<Sermon[]> {
@@ -17,32 +16,24 @@ export class LigonierScraper implements SermonScraper {
                 uri: `https://www.ligonier.org/learn/sermons/?page_sermons=${index}&sort=az`,
                 transform: function (body) {
                     return cheerio.load(body);
-                }
+                },
             };
 
             let $ = await request(options);
 
             $('ul.resource-list li').each(function (index, element) {
-                let title = $(element)
-                    .find('h2 a')
-                    .text()
-                    .trim();
-                let scripture = $(element)
-                    .find('p.meta')
-                    .text()
-                    .trim();
+                let title = $(element).find('h2 a').text().trim();
+                let scripture = $(element).find('p.meta').text().trim();
                 let url =
                     'https://www.ligonier.org' +
-                    $(element)
-                        .find('h2 a')
-                        .attr('href');
+                    $(element).find('h2 a').attr('href');
                 if (scripture) {
                     sermons.push({
                         title: title,
                         scripture: scripture,
                         author: 'R.C. Sproul',
-                        source: this.source,
-                        url: url
+                        source: `Ligonier Ministries`,
+                        url: url,
                     });
                 }
             });
@@ -51,4 +42,3 @@ export class LigonierScraper implements SermonScraper {
         return sermons;
     }
 }
-
